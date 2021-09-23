972402e21d4ea89df4558dbefc46acc5f20aadb4
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-09-23 18:13:21 -0500

    feat: 🎸 add connection to mongodb

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connectDB from 'utils/be/connect-db'
+
+connectDB()
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
---
+++ utils/be/variables.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const BE_MONGODB_URI = process.env.BE_MONGODB_URI
+
+module.exports = {
+  BE_MONGODB_URI
+}
