e590e80781af88f02b32e483484be080f883a410
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-09-24 09:43:04 -0500

    feat: 🎸 add PUT to User controller

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -19,18 +19,40 @@ const userRouter = async (req, res) => {
     }
   }
 
+  const PUT = async () => {
+    const { name } = body
+
+    try {
+      const updatedUser = {
+        name
+      }
+
+      await User.findByIdAndUpdate(id, updatedUser, {
+        new: true,
+        runValidators: true,
+        context: 'query'
+      })
+      res.status(200).json({ success: true, data: updatedUser })
+    } catch (e) {
+      res.status(404).json({ success: false, error: e.message })
+    }
+  }
+
   const DELETE = async () => {
     try {
       await User.findByIdAndRemove(id)
       res.status(204).end()
     } catch (e) {
-      res.status(404).json({ success: false, error: 'User not found' })
+      res.status(404).json({ success: false, error: e.message })
     }
   }
 
   switch (method) {
     case 'GET':
       await GET()
+      break
+    case 'PUT':
+      await PUT()
       break
     case 'DELETE':
       await DELETE()
